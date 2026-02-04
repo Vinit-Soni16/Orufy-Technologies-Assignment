ea7060ca5ae1328721cf4184ab4a4747e9fdf504
Vinit-Soni16 <israniprincy@gmail.com>
2026-02-04 16:29:59 +0530

    u

--- Backend/utils/emailService.js
+++ Backend/utils/emailService.js
@@ -12,8 +12,8 @@ function getTransporter() {
 
   transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: { user, pass },
     // Fail fast if connection hangs
     connectionTimeout: 10000,
